a90ea0ad3547697c427c0b50a4dbeac53389252c
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-04-02 00:14:11 +0200

    Add PasswordResetConfirmView function to confirm password reset

--- account/urls.py
+++ account/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/update/', ProfileUpdateView.as_view(), name='ProfileUpdate'),
     path('password-reset-request/', PasswordResetRequestView.as_view(), name='passwordResetRequest'),
+    path('password-reset-confirm/', PasswordResetConfirmView.as_view(), name='passwordResetConfirm'),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
