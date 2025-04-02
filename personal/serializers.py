4e1b7d2bcfc6891c8964159c490a2bd3fce29201
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-04-02 14:16:50 +0200

    Add PersonalSerializer for Personal model update

--- personal/serializers.py
+++ personal/serializers.py
@@ -1,8 +1,8 @@
+from personal.models import *
 from rest_framework import serializers
-from personal.models import Personal
 
 class PersonalSerializer(serializers.ModelSerializer):
     class Meta:
         model = Personal
-        fields = ['id', 'id_number', 'bio', 'country', 'district', 'sector', 'cell', 'village', 'date_of_birth', 'gender', 'profile_completed']
-        read_only_fields = ['id', 'profile_completed']
+        fields = ['id', 'id_number', 'bio', 'country', 'district', 'sector', 'cell', 'village', 'date_of_birth', 'gender', 'profile_completed', 'created_at', 'updated_at']
+        read_only_fields = ['profile_completed', 'created_at', 'updated_at']
