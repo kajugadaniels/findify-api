0859160985d0c9951e9a48ab18b7059d1c344d9b
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-04-02 13:58:42 +0200

    Create personal app URLs and route for updating personal user profile

--- personal/urls.py
+++ personal/urls.py
@@ -6,5 +6,5 @@ from django.conf.urls.static import static
 app_name = 'customer'
 
 urlpatterns = [
-    # 
+    path('update/<int:user_id>/', PersonalProfileUpdateView.as_view(), name='updatePersonalProfile'),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
