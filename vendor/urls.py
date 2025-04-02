46c48366aec950886c44ed29b718c852dd5ee8a2
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-04-02 14:36:11 +0200

    Add URL pattern for VendorProfileUpdateView endpoint

--- vendor/urls.py
+++ vendor/urls.py
@@ -6,5 +6,5 @@ from django.conf.urls.static import static
 app_name = 'vendor'
 
 urlpatterns = [
-    # 
+    path('update/<int:user_id>/', VendorProfileUpdateView.as_view(), name='vendorProfileUpdate'),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
