4967cf149e11fe3400a9e1a5e719da03721b344d
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-04-02 13:56:24 +0200

    Create PersonalSerializer for managing personal user profile data

--- static/serializers.py
+++ static/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
-from personal.models import *
+from personal.models import Personal
 
 class PersonalSerializer(serializers.ModelSerializer):
     class Meta:
         model = Personal
-        fields = '__all__'
-        read_only_fields = ['user', 'created_at', 'updated_at']
+        fields = ['id', 'id_number', 'bio', 'country', 'district', 'sector', 'cell', 'village', 'date_of_birth', 'gender', 'profile_completed']
+        read_only_fields = ['id', 'profile_completed']
