262d2cb4a8b07733337289d1059bfd2ef6b1134f
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-04-02 13:48:19 +0200

    Setup vendor app in main app

--- api/urls.py
+++ api/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('account.urls')),
+    path('api/vendor/', include('vendor.urls')),
     path('api/customer/', include('personal.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
