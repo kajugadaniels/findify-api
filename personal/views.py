d629620d6bc99823c89a5ad0d80a5d60395052be
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-04-02 13:57:37 +0200

    Create PersonalSerializer for managing personal user profile data

--- personal/views.py
+++ personal/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from personal.models import Personal
+from personal.serializers import *
+from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
+from account.models import *
 
-# Create your views here.
+class PersonalProfileUpdateView(APIView):
+    """
+    View to update personal profile using user ID.
+    Only authenticated users can access.
+    """
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request, user_id, *args, **kwargs):
+        user = get_object_or_404(User, pk=user_id, role='Personal')
+        personal_profile, created = Personal.objects.get_or_create(user=user)
+        serializer = PersonalSerializer(instance=personal_profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                "detail": "Personal profile updated successfully.",
+                "personal_profile": serializer.data
+            }, status=status.HTTP_200_OK)
+        return Response({
+            "detail": "Update failed.",
+            "errors": serializer.errors
+        }, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, user_id, *args, **kwargs):
+        user = get_object_or_404(User, pk=user_id, role='Personal')
+        personal_profile, created = Personal.objects.get_or_create(user=user)
+        serializer = PersonalSerializer(instance=personal_profile, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                "detail": "Personal profile updated successfully.",
+                "personal_profile": serializer.data
+            }, status=status.HTTP_200_OK)
+        return Response({
+            "detail": "Update failed.",
+            "errors": serializer.errors
+        }, status=status.HTTP_400_BAD_REQUEST)
