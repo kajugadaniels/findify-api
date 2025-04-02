f3a6bfc8b26f281fefaaa9013e8529cc8914cf2c
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-04-02 14:41:45 +0200

    Update VendorSerializer to include nested User data

--- vendor/serializers.py
+++ vendor/serializers.py
@@ -1,8 +1,11 @@
 from vendor.models import *
 from rest_framework import serializers
+from account.serializers import UserSerializer
 
 class VendorSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
     class Meta:
         model = Vendor
-        fields = ['id', 'company_name', 'tin_number', 'business_type', 'email', 'phone', 'website', 'address', 'country', 'district', 'sector', 'cell', 'village', 'is_verified', 'created_at', 'updated_at']
+        fields = ['id', 'user', 'company_name', 'tin_number', 'business_type', 'email', 'phone', 'website', 'address', 'country', 'district', 'sector', 'cell', 'village', 'is_verified', 'created_at', 'updated_at']
         read_only_fields = ['is_verified', 'created_at', 'updated_at']
