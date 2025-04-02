473b390b857ab45ba9e206d1c2c75221b081ccda
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-04-02 13:41:17 +0200

    Setup personal in main app

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('superadmin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('api/auth/', include('account.urls')),
+    path('api/customer/', include('personal.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
